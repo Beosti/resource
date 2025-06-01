fcb1e943aa02f1635d30c93195006a66aed28c40
beosti <beostishi@gmail.com>
2025-06-01 19:17:18 +0200

    added ressource widget and tooltips

--- BaseUiScene.gd
+++ BaseUiScene.gd
@@ -12,6 +12,10 @@ var copperMineScene = preload("res://coppermine/CopperClickerScene.tscn");
 @export var copperClickerScene: CopperClickerClass;
 @export var coalClickerScene: CoalClickerClass;
 
+@export var stoneResourceLabel: Label;
+@export var coalResourceLabel: Label;
+@export var rawCopperResourceLabel: Label;
+
 @onready var mineContainer = $Mines;
 
 func _ready():
@@ -29,11 +33,14 @@ func _ready():
 	coalMineButton.show();
 
 func change_stone_mine_text():
-	stoneMineButton.text = "Stone Mine\nStone: %s" %stoneClickerScene.stone; 
+	stoneResourceLabel.text = "Stone: %s" %stoneClickerScene.stone;
+	stoneResourceLabel.tooltip_text = "Generating %s stone per second" % stoneClickerScene.diggers;
 func change_copper_mine_text():
-	copperMineButton.text = "Copper Mine\nRaw copper: %s" %copperClickerScene.copper;
+	rawCopperResourceLabel.text = "Raw copper: %s" %copperClickerScene.copper;
+	rawCopperResourceLabel.tooltip_text = "Generating %s raw copper per second" % copperClickerScene.diggers;
 func change_coal_mine_text():
-	coalMineButton.text = "Coal Mine\nCoal: %s" %coalClickerScene.resource;
+	coalResourceLabel.text = "Coal: %s" %coalClickerScene.resource;
+	coalResourceLabel.tooltip_text = "Generating %s coal per second" % coalClickerScene.diggers;
 
 func main_menu():
 	get_node("Mines/StoneClicker").hide();
@@ -42,6 +49,7 @@ func main_menu():
 	stoneMineButton.show();
 	copperMineButton.show();
 	coalMineButton.show();
+	get_node("PanelContainer").show();
 
 func _on_stone_mine_button_button_down() -> void:
 	get_node("Mines/StoneClicker").show();
@@ -56,11 +64,13 @@ func _on_coal_mine_button_button_down() -> void:
 	hide_buttons();
 
 func hide_buttons():
+	get_node("PanelContainer").hide();
 	stoneMineButton.hide();
 	copperMineButton.hide();
 	coalMineButton.hide();
 
 func show_buttons():
+	get_node("PanelContainer").show();
 	stoneMineButton.show();
 	copperMineButton.show();
 	coalMineButton.show();
